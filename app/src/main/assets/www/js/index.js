4cf6d9e3c589a70d7596a5d4b4a40f208769c957
srenaudon <srenaudon@users.noreply.github.com>
2015-03-19 15:44:12 +0100

    Update index.js
    tiling

--- app/src/main/assets/www/js/index.js
+++ app/src/main/assets/www/js/index.js
@@ -205,6 +205,13 @@ layer = _toPolygon(bso);
 	 L.esri.dynamicMapLayer( config.map.afgha.mapUrl).addTo(map);
 
  }
+ 
+ /*
+  L.tileLayer.wms("http://192.168.246.20/arcgis/services/Contestreduit/MapServer/WMSServer", {
+    layers: '0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33',
+}).addTo(map);
+ 
+ */
 
 
 //location control
