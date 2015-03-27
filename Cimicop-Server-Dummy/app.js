0c1658dc27986afb547acc93f3fa847a9b2fcb50
Sylvain Renaudon <sylvain@nullepart.net>
2015-03-27 16:40:43 +0100

    Check DTO format

--- Cimicop-Server-Dummy/app.js
+++ Cimicop-Server-Dummy/app.js
@@ -1,13 +1,16 @@
 var restify = require('restify');
 
 
-
 function respond(req, res, next) {
   res.send("hello I'm CIMICOP");
   next();
 }
 
-var server = restify.createServer();
+server = restify.createServer({
+  name: 'cimicop dummy',
+  version: '0.1.0'
+});
+
 server.use(restify.bodyParser({ mapParams: false }));
 
 
@@ -18,14 +21,124 @@ server.post('/cimicop/situation/tocivilian', function create(req, res, next) {
 
   console.log("header is "+req.header('content-type'));
 
-	//check req.is('application/json');
-  console.log("body is "+req.body);      // your JSON
-  for(var i in req.body){
-  	console.log("- "+i);
+  //check content Type
+  var report="";
+  var valid = true;
+  try{
+    if(!req.is('application/json')){
+      valid = false;
+      report+="- content type is not application/json "+" || ";
+    }
+
+//check DTO
+/* Valid DTO Sample
+{
+  "id":"new",
+  "type": "event",
+    "subtype": "bomb",
+  "name":"marker-02",
+    "description":"a desc",
+  "shape":{
+  "type" : "ponctual",
+  "coords":"[{\"lat\":\"48.85\",\"lon\":\"2.5\"}]"
   }
-  res.send(req.body);    // echo the result back
- //   res.send(200);
- });
+}
+
+*/
+var report = req.body;
+
+if(typeof(report.id) == "undefined"){
+  valid = false;
+  report+="- report.id is mandatory " +" || ";
+}else{
+  if(report.id != "new"){
+    valid = false;
+    report+="- report.id should be 'new' but is " +report.id +" || ";
+  }
+}
+
+if(typeof(report.type) == "undefined"){
+  valid = false;
+  report+="- report.type is mandatory " +" || ";
+}
+
+if( report.type != "event"){
+  valid = false;
+  report+="- (report.type should be an 'event' but is " +report.type +" || ";
+}
+
+
+if(typeof(report.subtype) == "undefined"){
+  valid = false;
+  report+="- report.subtype is mandatory "  +" || ";
+}else{
+
+  if( report.subtype != "armed-group"
+   && report.subtype != "bomb"
+   && report.subtype != "death"
+   && report.subtype != "injured"
+   && report.subtype != "jeep"
+   && report.subtype != "kidnap"
+   && report.subtype != "other" 
+   && report.subtype != "riot" 
+   && report.subtype != "tank" 
+   ){
+    valid = false;
+  report+="- report.subtype is unknown " +report.subtype +" || ";
+}
+}
+
+
+
+if(typeof(report.name) == "undefined"){
+  valid = false;
+  report+="- report.name is mandatory " +" || ";
+}
+
+if(typeof(report.description) == "undefined"){
+  valid = false;
+  report+="- report.description is mandatory "  +" || ";
+}
+
+if(typeof(report.shape) == "undefined"){
+  valid = false;
+  report+="- report.shape is mandatory " +" || ";
+}else{
+
+  if(report.shape.type != "ponctual"){
+    valid = false;
+    report+="- (report.shape.type should be a ponctual but is " +report.shape.type +" || ";
+  }
+  var coords = JSON.parse(report.shape.coords);
+
+  if(coords.length != 1){
+    valid = false;
+    report+="- (report.shape.coords should be one and only one point "+" || ";
+  }else{
+
+    if(typeof(coords[0].lat) == "undefined"){
+      valid = false;
+      report+="- (report.shape.coords should have latitude "+" || ";
+    }
+
+    if(typeof(coords[0].lon) == "undefined"){
+      valid = false;
+      report+="- (report.shape.coords should have longitude "+" || ";
+    }
+  }
+}
+
+}catch(e){
+  valid = false;
+  report+="BOOM :  "+e;
+
+}
+if(valid){
+  res.send(200,req.body);  
+}else{
+  res.send(500,report+"    "+req.body);  
+}
+});
 
 
 server.listen(90, function() {
