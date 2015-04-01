80c66a8493cfe91e624b5aff54ace2ab0fa191be
sylvain <renaudon.sylvain@gmail.com>
2015-04-01 22:54:51 +0200

    cleanUp

--- app/src/main/assets/www/js/mapview.js
+++ app/src/main/assets/www/js/mapview.js
@@ -1,4 +1,5 @@
 /**********     addBso     *********/
+
 //add bso on map
 var addBso = function(bsoJson){
 
@@ -10,7 +11,7 @@ var addBso = function(bsoJson){
 //@private
 var _addBso = function(bso){
  var layer;
- if(bso.shape.type == "ponctual"){
+ if(bso.type == "event"){
 
   layer = _toMarker(bso);
   if(layer != undefined){
@@ -18,9 +19,9 @@ var _addBso = function(bso){
     cache.put(bso.id,layer);
   }
 
-}else if(bso.shape.type == "area"){
+}else if(bso.type == "area"){
 
-layer = _toPolygon(bso);
+  layer = _toPolygon(bso);
   if(layer != undefined){
     layer.addTo(map);
     cache.put(bso.id,layer);
@@ -29,7 +30,9 @@ layer = _toPolygon(bso);
 }
 
 }
+
 /**********     removeBso     *********/
+
   //remove bso from map
   var removeBso = function(id){
 
@@ -39,6 +42,7 @@ layer = _toPolygon(bso);
  }
 
  /**********     updateBso     *********/
+
  //update bso from map
  var updateBso = function(bsoJson){
    _debug("updateBso");
@@ -61,100 +65,100 @@ layer = _toPolygon(bso);
 
     }
 
-    /**********    transform ponctual Bso to leaflet marker     *********/
+/**********    transform ponctual Bso to leaflet marker     *********/
 //define icons
 
 var markerIcons = {};
 var icon = L.icon({
-    iconUrl: 'icon/armed-group-marker.png',
-    popupAnchor: [1, -16],
-    iconSize:     [32, 32]
-    });
+  iconUrl: 'icon/armed-group-marker.png',
+  popupAnchor: [1, -16],
+  iconSize:     [32, 32]
+});
 markerIcons["armed-group"]=icon;
 
 icon = L.icon({
-    iconUrl: 'icon/death-marker.png',
-    popupAnchor: [1, -16],
-    iconSize:     [32, 32]
-    });
+  iconUrl: 'icon/death-marker.png',
+  popupAnchor: [1, -16],
+  iconSize:     [32, 32]
+});
 markerIcons["death"]=icon;
 
 icon = L.icon({
-    iconUrl: 'icon/injured-marker.png',
-    popupAnchor: [1, -16],
-    iconSize:     [32, 32]
-    });
+  iconUrl: 'icon/injured-marker.png',
+  popupAnchor: [1, -16],
+  iconSize:     [32, 32]
+});
 markerIcons["injured"]=icon;
 
 icon = L.icon({
-    iconUrl: 'icon/kidnap-marker.png',
-    popupAnchor: [1, -16],
-    iconSize:     [32, 32]
-    });
+  iconUrl: 'icon/kidnap-marker.png',
+  popupAnchor: [1, -16],
+  iconSize:     [32, 32]
+});
 markerIcons["kidnap"]=icon;
 
 icon = L.icon({
-    iconUrl: 'icon/other-marker.png',
-    popupAnchor: [1, -16],
-    iconSize:     [32, 32]
-    });
+  iconUrl: 'icon/other-marker.png',
+  popupAnchor: [1, -16],
+  iconSize:     [32, 32]
+});
 markerIcons["other"]=icon;
 
 icon = L.icon({
-    iconUrl: 'icon/riot-marker.png',
-    popupAnchor: [1, -16],
-    iconSize:     [32, 32]
-    });
+  iconUrl: 'icon/riot-marker.png',
+  popupAnchor: [1, -16],
+  iconSize:     [32, 32]
+});
 markerIcons["riot"]=icon;
 
 icon = L.icon({
-    iconUrl: 'icon/tank-marker.png',
-    popupAnchor: [1, -16],
-    iconSize:     [32, 32]
-    });
+  iconUrl: 'icon/tank-marker.png',
+  popupAnchor: [1, -16],
+  iconSize:     [32, 32]
+});
 markerIcons["tank"]=icon;
 
 icon = L.icon({
-    iconUrl: 'icon/bomb-marker.png',
-    popupAnchor: [1, -16],
-    iconSize:     [32, 32]
-    });
+  iconUrl: 'icon/bomb-marker.png',
+  popupAnchor: [1, -16],
+  iconSize:     [32, 32]
+});
 markerIcons["bomb"]=icon;
 
 
-        //@private
-        var _toMarker = function(bso){
-         try{
-            return _bindPopup(L.marker(L.latLng(bso.shape.coords[0].lat, bso.shape.coords[0].lon), {icon: markerIcons[bso.subtype]}),bso);
+//@private
+var _toMarker = function(bso){
+ try{
+  return _bindPopup(L.marker(L.latLng(bso.shape.coords[0].lat, bso.shape.coords[0].lon), {icon: markerIcons[bso.subtype]}),bso);
+  }catch(reason){
+  _log("ERROR: unable to transform ponctual to Marker with reason :" +reason);
+  }
+}
 
-          }catch(reason){
-            _log("ERROR: unable to transform ponctual to Marker with reason :" +reason);
-          }
-       }
 
-    /**********   transform area Bso to leaflet polygone     *********/
+/**********   transform area Bso to leaflet polygone     *********/
 
-        //@private
-        var _toPolygon = function(bso){
-         try{
-          var edges=[];
-          for (var i=0;i<bso.shape.coords.length;i++){
-            if(bso.shape.coords[i] !== null){
-                edges.push(L.latLng(bso.shape.coords[i].lat, bso.shape.coords[i].lon));
-            }
-          }
-          var color;
-           if(bso.subtype =='danger'){color = "#ff0000";}
-           else if(bso.subtype =='warning'){color = "#FF8100";}
-           else if(bso.subtype =='safe'){color = "#00b35a";}
-           else{color = "#ff0000";}
-          return _bindPopup(L.polygon(edges,{weight:1,color:color}),bso);
+//@private
+var _toPolygon = function(bso){
+ try{
+  var edges=[];
+  for (var i=0;i<bso.shape.coords.length;i++){
+    if(bso.shape.coords[i] !== null){
+      edges.push(L.latLng(bso.shape.coords[i].lat, bso.shape.coords[i].lon));
+    }
+  }
+  var color;
+  if(bso.subtype =='danger'){color = "#ff0000";}
+  else if(bso.subtype =='warning'){color = "#FF8100";}
+  else if(bso.subtype =='safe'){color = "#00b35a";}
+  else{color = "#ff0000";}
+  return _bindPopup(L.polygon(edges,{weight:1,color:color}),bso);
 
-          }catch(reason){
-            _log("ERROR: unable to transform area to polygone with reason :" +reason);
-          }
-       }
-        //add popup info to bso
-       var _bindPopup = function(layer,bso){
-        return layer.bindPopup("<b>"+bso.name+"</b><br>"+bso.description);
-       }
+}catch(reason){
+  _log("ERROR: unable to transform area to polygone with reason :" +reason);
+}
+}
+//add popup info to bso
+var _bindPopup = function(layer,bso){
+  return layer.bindPopup("<b>"+bso.name+"</b><br>"+bso.description);
+}
